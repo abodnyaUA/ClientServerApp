d7613ae8a6e799e8c2483cc2f3b7a7d06c295083
Aleksey Bodnya <abodnya@abodnya-elementary.(none)>
2014-04-07 21:40:24 +0300

    Code clean-up. Refactor

--- PHP/Templating/TemplateProduct/contentProcessing.php
+++ PHP/Templating/TemplateProduct/contentProcessing.php
@@ -7,14 +7,12 @@
 		<?=$page->content?>
 	"</span>
 	<h4>Screenshots:</h4>
-	<?php 
-		for ($i = 1; $i < 4; $i++) {
-		$adress = "/DataBase/images/".$page->name."/screen_".$i.".png";
-	?>
+	<?php for ($i = 1; $i < 4; $i++) : ?>
+	<?php $adress = "/DataBase/images/".$page->name."/screen_".$i.".png"; ?>
 		<div class='screenshot'>
 			<a href='<?=$adress?>' rel='lightbox[screen]' title=''>
 				<img src='<?=$adress?>' class='screenshot'>
 			</a>
 		</div>";
-	<?php } ?>
+	<?php endfor; ?>
 </div>
